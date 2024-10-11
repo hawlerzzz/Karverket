22adf7d097815a4f83f998bd32aa4b13e765d896
Ayub <ayuubomer88@gmail.com>
2024-10-11 11:23:33 +0200

    - Lagt funksjonalitet til login og registrering - lagt til nye kontrollere - lagt til modeller for login og signup - lagt til exceptions modeller

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Karverket.Controllers
+{
+    public class LoginController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return Json("login");
+        //}
+        [HttpGet]
+        public IActionResult Index(string error)
+        {
+            ViewData["error"] = error;
+            return View();
+        }
+
+        public IActionResult Login(string error)
+        {
+            ViewData["error"] = error;
+            return View();
+        }
+    }
+}
