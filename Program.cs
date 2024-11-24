5abda11400ee546bf43c2cd821d1236d4c9906e1
Ayub <ayuubomer88@gmail.com>
2024-11-24 10:28:09 +0100

    compose

--- Program.cs
+++ Program.cs
@@ -37,6 +37,13 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+// Automatically apply migrations
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
