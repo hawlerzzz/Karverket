a293ba95207cc39fe0baa1b8eff68e8a0c01aba5
Ahmed Anwar <144031709+ahmedanwar2@users.noreply.github.com>
2024-11-25 12:39:09 +0100

    Delete Dal/GeoChange.cs

--- Dal/GeoChange.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Karverket.DAL
-{
-    public class GeoChange
-    {
-        public string Id { get; set; }
-        public string GeoJson { get; set; }
-        public string Description { get; set; }
-    }
-}
